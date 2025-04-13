7e4de4257ec7ad0bcea627cc6cf4f8415696579a
Ryan G <hatsunemiku@pipebomb.icu>
2025-04-13 18:50:51 -0500

    implement a method for checking point distance advancements. there's probably a vanilla way to do this, but I don't care enough to find it.

--- src/main/java/ing/pipebomb/alphamericaadv/AlphamericaAdv.java
+++ src/main/java/ing/pipebomb/alphamericaadv/AlphamericaAdv.java
@@ -1,5 +1,8 @@
 package ing.pipebomb.alphamericaadv;
 
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -8,6 +11,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
+import java.util.function.Supplier;
+
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(AlphamericaAdv.MODID)
 public class AlphamericaAdv
@@ -15,13 +20,20 @@ public class AlphamericaAdv
     // Define mod id in a common place for everything to reference
     public static final String MODID = "alphamericaadv";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<LootItemConditionType> LOOT_PREDICATE = DeferredRegister.create(BuiltInRegistries.LOOT_CONDITION_TYPE,MODID);
+
+    public static final Supplier<LootItemConditionType> DISTANCE_TO = LOOT_PREDICATE.register("distance_to", () -> new LootItemConditionType(DistanceCheck.CODEC));
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public AlphamericaAdv(IEventBus modEventBus, ModContainer modContainer)
     {
         LOGGER.info("Advancements mod loaded");
-    }
 
+        LOOT_PREDICATE.register(modEventBus);
+
+        //NeoForge.EVENT_BUS.register(this);
+    }
 }
